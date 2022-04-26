32357f46bb85746c54286be71f22ba40cc58ebe4
Fredrik Peteri Harr <fredrik@peteri.se>
2022-04-26 21:56:44 +0200

    Adds Express server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+const path = require("path");
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
+app.listen(process.env.PORT || 3000);
