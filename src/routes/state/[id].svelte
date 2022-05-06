52c2f6b2fe2e4f4d2793605e9ddec069aacdf6f4
Fredrik Peteri Harr <fredrik@peteri.se>
2022-05-06 10:38:55 +0200

    POST new state

--- src/routes/state/[id].svelte
+++ src/routes/state/[id].svelte
@@ -4,17 +4,29 @@
 	export let state: State;
 </script>
 
-<h1>{state.name}</h1>
-<h4>{state.uuid}</h4>
+{#if state.name}
+	<h1>{state.name}</h1>
+{/if}
+
+{#if state.uuid}
+	<h4>{state.uuid}</h4>
+{/if}
+
 PWMs:
-<ol>
-	{#each state.pwms as pwm}
-		<li>{pwm}</li>
-	{/each}
-</ol>
+{#if state.pwms}
+	<ol>
+		{#each state.pwms as pwm}
+			<li>{pwm}</li>
+		{/each}
+	</ol>
+{/if}
+
+
 Servos:
-<ol>
-	{#each state.servos as servo}
-		<li>ID: {servo.id}, Axis: {servo.axis}, Speed: {servo.speed}, {servo.min}-{servo.max}, start: {servo.startValue}</li>
-	{/each}
-</ol>
+{#if state.servos}
+	<ol>
+		{#each state.servos as servo}
+			<li>ID: {servo.id}, Axis: {servo.axis}, Speed: {servo.speed}, {servo.min}-{servo.max}, start: {servo.startValue}</li>
+		{/each}
+	</ol>
+{/if}
