d94d9f45e2902ea33233b27821abc574e1ab7e89
Fredrik Peteri Harr <fredrik@peteri.se>
2022-05-13 10:38:28 +0200

    Adds receiver and Arduino code

---
+++ arduino/robotArmController/robotArmController.ino
@@ -0,0 +1,120 @@
+/*
+ * Robot Arm Controller for Arduino
+ * 
+ * Reads n-amount of servo values from serial and
+ * sets the servos accordingly.
+ * 
+ *
+ * Signal:
+ *    BYTE | VALUE | NOTE
+ *    -----------------------
+ *      0  |   2   | Start
+ *      1  | 0-255 | Number of servos
+ *  2:end-1| 0-255 | PWM-value
+ *     end | 0-255 | Checksum = sum(PWM-values) % 256
+ */
+
+#include <Servo.h>
+
+typedef struct {
+  byte pwm;
+  byte dir;
+} Motor;
+
+const int SERVO_START_PIN = 8;
+const int SERVOS = 6;
+const int BAUD_RATE = 19200;
+const byte STX = 2;
+const int BAD_CHECKSUM_LED_PIN = 2;
+
+const byte MOTOR_1_DIR = 4;
+const byte MOTOR_2_DIR = 7;
+const byte MOTOR_1_PWM = 5;
+const byte MOTOR_2_PWM = 6;
+
+long last_bad_checksum = millis();
+byte motors[4] = {0};
+byte pwms[SERVOS] = {127};
+Servo servo[SERVOS];
+
+void setup() {
+	pinMode(BAD_CHECKSUM_LED_PIN, OUTPUT);
+	for (int i = 0; i < SERVOS; i++) {
+		servo[i].attach(SERVO_START_PIN + i);
+	}
+  pinMode(MOTOR_1_DIR, OUTPUT);
+  pinMode(MOTOR_2_DIR, OUTPUT);
+  pinMode(MOTOR_1_PWM, OUTPUT);
+  pinMode(MOTOR_2_PWM, OUTPUT);
+	Serial.begin(BAUD_RATE);
+}
+
+byte nextByte() {
+	byte b = Serial.read();
+	while (b == -1) {
+		b = Serial.read();
+		delay(10);
+	}
+	return b;
+}
+
+void clearMessage() {
+	memset(pwms, 0, SERVOS);
+}
+
+void waitForSTX() {
+	byte temp = nextByte();
+	while (temp != STX) {
+		temp = nextByte();
+		delay(10);
+	}
+}
+
+bool readSerial(byte *pwms) {
+	byte n = nextByte();
+
+	byte temp[n] = {0};
+	byte checksum = 0;
+	for (int i = 0; i < n; i++) {
+		temp[i] = nextByte();
+		checksum += temp[i];
+	}
+
+	byte received_checksum = nextByte();
+	if (received_checksum != checksum) {
+		Serial.print("Bad checksum, received: ");
+		Serial.print(received_checksum);
+		Serial.print(", calculated: ");
+		Serial.println(checksum);
+		last_bad_checksum = millis();
+		return false;
+	}
+
+	memcpy(pwms, temp, SERVOS);
+  memcpy(motors, &temp[SERVOS], 4);
+	return true;
+}
+
+void updateServos(byte *pwms) {
+	for (int i = 0; i < SERVOS; i++) {
+		servo[i].writeMicroseconds(map(pwms[i], 0, 255, 500, 2500));
+	}
+}
+
+void updateMotors(byte *motors) {
+  analogWrite(MOTOR_1_PWM, motors[0]);
+  digitalWrite(MOTOR_1_DIR, motors[1]);
+  analogWrite(MOTOR_2_PWM, motors[2]);
+  digitalWrite(MOTOR_2_DIR, motors[3]);
+}
+
+void loop() {
+	clearMessage();
+	waitForSTX();
+	if (readSerial(pwms)) {
+		updateServos(pwms);
+    updateMotors(motors);
+	}
+
+	digitalWrite(BAD_CHECKSUM_LED_PIN, last_bad_checksum + 100 <= millis() ? LOW : HIGH);
+}
---
+++ receiver/receiver.py
@@ -0,0 +1,72 @@
+import asyncio
+import sys
+from time import time
+
+import serial
+import websockets
+import json
+
+com_port = None
+com_rate = 19200
+socket_port = None
+ser = None
+
+
+try:
+	com_port = sys.argv[1]
+	socket_port = sys.argv[2]
+except IndexError as ex:
+	print(f"USAGE: {sys.argv[0]} COM_PORT SOCKET_PORT", file=sys.stderr)
+	exit(1)
+
+
+async def main():
+	with serial.Serial(com_port, com_rate, write_timeout=0, timeout=0) as ser:
+		async def read_serial_to_socket(websocket):
+			if ser.in_waiting > 0:
+				msg = ""
+				b = ser.read().decode("ascii")
+				while b != "\n":
+					msg += b
+					b = ser.read().decode("ascii")
+				await websocket.send(msg)
+
+		async def socket_handler(websocket):
+			last_update = time()
+			async for message in websocket:
+				if time() < last_update + 0.02:
+					continue
+				last_update = time()
+
+				message = json.loads(message)
+				
+				pwms = [0, 0, 0, 0, 0, 0]
+				for i, s_pwm in enumerate(message["servos"]):
+					pwms[i] = s_pwm
+
+				motors = []
+				for m_pwm, m_dir in message["motors"]:
+					motors.append(m_pwm)
+					motors.append(255 if m_dir else 0)
+
+				if len(pwms) < 1:
+					continue
+
+				data_length = len(pwms)+len(motors)
+				buff = bytearray(3+data_length)
+				buff[0] = 2  # STX
+				buff[1] = data_length  # Number bytes
+				buff[2: 2+len(pwms)] = bytearray(pwms)  # PWMs
+				buff[2+len(pwms): -1] = bytearray(motors)  # Motors
+				buff[-1] = sum(buff[2:-1]) % 256  # Checksum
+				ser.write(buff)
+
+				await read_serial_to_socket(websocket)
+
+		async with websockets.serve(socket_handler, "localhost", socket_port):
+			await asyncio.Future()
+
+try:
+	asyncio.run(main())
+except KeyboardInterrupt:
+	exit(0)
