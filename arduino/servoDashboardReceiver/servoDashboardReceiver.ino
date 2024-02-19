586df979041c45e5c57cf0b5f7920ace5beff04c
Fredrik Peteri Harr <fredrik@peteri.se>
2024-02-19 20:23:24 +0100

    New communication protocol

--- arduino/servoDashboardReceiver/servoDashboardReceiver.ino
+++ arduino/servoDashboardReceiver/servoDashboardReceiver.ino
@@ -2,151 +2,127 @@
  * Servo Dashboard Receiver for Arduino UNO
  */
 
-#include <Servo.h>
+typedef enum State {
+	BOOT = 'B',
+	START = 'S',
+	READ_COMMAND = 'R',
+	READ_PARAMETER = 'P',
+	EXECUTE_COMMAND = 'E'
+} State;
 
-// true: Usees pin 4, 5, 6, 7 as direction and PWM pins for motor control.
-// false: Uses Pin 5 and 6 as regular PWM outputs for motor control using ESC's.
-#define USE_H_BRIDGE false
-#define FAILSAFE_MS 50
+enum Commands {
+	C_ECHO = 'E'
+};
 
-const int SERVO_PINS[9] = {8, 9, 10, 11, 12, 13, A0, A1, A2};
-const int SERVOS = 9;
 const int BAUD_RATE = 19200;
-const byte STX = 2;
-const int BAD_CHECKSUM_LED_PIN = 2;
-const int FAILSAFE_LED_PIN = A3;
-const int CUSTOM_PIN = 3;
+const int DEBUG_PIN = 13;
 
-const byte MOTOR_1_DIR = 4;
-const byte MOTOR_2_DIR = 7;
-const byte MOTOR_1_PWM = 5;
-const byte MOTOR_2_PWM = 6;
-
-long last_bad_checksum = millis();
-long failsafe_timer = 0L;
-byte motors[2] = {0};
-Servo motors_servo[2];
-byte pwms[SERVOS] = {127};
-Servo servo[SERVOS];
-byte custom = 0;
+uint8_t state = BOOT;
+uint8_t command = NULL;
+uint16_t parameter = 0;
+uint8_t parameter_i = 0;
 
 void setup() {
-	pinMode(BAD_CHECKSUM_LED_PIN, OUTPUT);
-	pinMode(CUSTOM_PIN, OUTPUT);
-	pinMode(FAILSAFE_LED_PIN, OUTPUT);
-	digitalWrite(FAILSAFE_LED_PIN, HIGH);
-
-	if (USE_H_BRIDGE) {
-		pinMode(MOTOR_2_DIR, OUTPUT);
-		pinMode(MOTOR_2_DIR, OUTPUT);
-		pinMode(MOTOR_1_PWM, OUTPUT);
-		pinMode(MOTOR_2_PWM, OUTPUT);
-	} else {
-		motors_servo[0].writeMicroseconds(1500);
-		motors_servo[1].writeMicroseconds(1500);
-		motors_servo[0].attach(MOTOR_1_PWM);
-		motors_servo[1].attach(MOTOR_2_PWM);
-	}
+	pinMode(DEBUG_PIN, OUTPUT);
+	digitalWrite(DEBUG_PIN, LOW);
 
 	Serial.begin(BAUD_RATE);
 }
 
-byte nextByte() {
-	byte b = Serial.read();
-	while (b == -1) {
-		failsafe();
-		b = Serial.read();
-		delay(10);
-	}
-	return b;
-}
+State state_boot(void) {
+	// Serial.println("STATE: BOOT");
+	static bool blink;
+	blink = !blink;
+	digitalWrite(DEBUG_PIN, blink);
 
-void failsafe() {
-	if (failsafe_timer + FAILSAFE_MS < millis()) {
-		digitalWrite(FAILSAFE_LED_PIN, HIGH);
-		if (!USE_H_BRIDGE) {
-			motors_servo[0].writeMicroseconds(1500);
-			motors_servo[1].writeMicroseconds(1500);
-		}
-	} else {
-		digitalWrite(FAILSAFE_LED_PIN, LOW);
-	}
-}
-
-void clearMessage() {
-	memset(pwms, 0, SERVOS);
-}
-
-void waitForSTX() {
-	byte temp = nextByte();
-	while (temp != STX) {
-		failsafe();
-		temp = nextByte();
-		delay(10);
-	}
-}
-
-bool readSerial() {
-	byte n = nextByte();
-
-	byte temp[n] = {0};
-	byte checksum = 0;
-	for (int i = 0; i < n; i++) {
-		temp[i] = nextByte();
-		checksum += temp[i];
-	}
-
-	byte received_checksum = nextByte();
-	if (received_checksum != checksum) {
-		Serial.print("Bad checksum, received: ");
-		Serial.print(received_checksum);
-		Serial.print(", calculated: ");
-		Serial.println(checksum);
-		last_bad_checksum = millis();
-		return false;
-	}
-
-	failsafe_timer = millis();
-
-	memcpy(pwms, temp, SERVOS);
-	memcpy(motors, &temp[SERVOS], 4);
-	custom = temp[SERVOS + 4];
-	return true;
-}
-
-void updateServos(byte *pwms) {
-	for (int i = 0; i < SERVOS; i++) {
-		servo[i].writeMicroseconds(map(pwms[i], 0, 255, 500, 2500));
-		if (!servo[i].attached()) {
-			servo[i].attach(SERVO_PINS[i]);
+	if (Serial.available()) {
+		uint8_t data = Serial.read();
+		if (data == START) {
+			return START;
 		}
 	}
+
+	return BOOT;
 }
 
-void updateMotors(byte *motors) {
-	if (USE_H_BRIDGE) {
-		analogWrite(MOTOR_1_PWM, motors[0]);
-		digitalWrite(MOTOR_1_DIR, motors[1]);
-		analogWrite(MOTOR_2_PWM, motors[2]);
-		digitalWrite(MOTOR_2_DIR, motors[3]);
-	} else {
-		motors_servo[0].writeMicroseconds(map(motors[0], 0, 255, 1500, motors[1] ? 2500 : 500));
-		motors_servo[1].writeMicroseconds(map(motors[2], 0, 255, 1500, motors[3] ? 2500 : 500));
+State state_start(void) {
+	// Serial.println("STATE: START");
+	digitalWrite(DEBUG_PIN, LOW);
+	return READ_COMMAND;
+}
+
+State state_read_command(void) {
+	// Serial.println("STATE: READ COMMAND");
+	if (Serial.available()) {
+		command = Serial.read();
+		switch (command) {
+			case C_ECHO:
+				Serial.print(char(C_ECHO));
+				return READ_PARAMETER;
+			default: 
+				return READ_COMMAND;
+		}
 	}
+	return READ_COMMAND;
 }
 
-void updateCustom(byte custom) {
-	digitalWrite(CUSTOM_PIN, custom > 0);
+State state_read_parameter(void) {
+	// Serial.println("STATE: READ PARAMETER");
+	digitalWrite(DEBUG_PIN, HIGH);
+	if (parameter_i == 0) {
+		parameter = 0;
+	}
+
+	if (Serial.available()) {
+		uint8_t data = Serial.read();
+		if (data < 48 || data > 57) {
+			return READ_PARAMETER;
+		}
+
+		parameter += (data - 48) * ceil(pow(10.0, 2.0 - parameter_i));
+
+		Serial.print(char(data));
+		if (parameter_i >= 2) {
+			parameter_i = 0;
+			Serial.println("");
+			return EXECUTE_COMMAND;
+		}
+
+		parameter_i++;
+	}
+	return READ_PARAMETER;
+}
+
+State state_execute_command(void) {
+	// Serial.println("STATE: EXECUTE COMMAND");
+	switch(command) {
+		case C_ECHO:
+			Serial.print("echo(");
+			Serial.print(parameter);
+			Serial.println(")");
+			break;
+	}
+
+	return START;
 }
 
 void loop() {
-	clearMessage();
-	waitForSTX();
-	if (readSerial()) {
-		updateServos(pwms);
-		updateMotors(motors);
-		updateCustom(custom);
+	switch(state) {
+		case BOOT:
+			state = state_boot();
+			delay(100);
+			break;
+		case START:
+			state = state_start();
+			break;
+		case READ_COMMAND:
+			state = state_read_command();
+			break;
+		case READ_PARAMETER:
+			state = state_read_parameter();
+			break;
+		case EXECUTE_COMMAND:
+			state = state_execute_command();
+			break;
 	}
-
-	digitalWrite(BAD_CHECKSUM_LED_PIN, last_bad_checksum + 100 <= millis() ? LOW : HIGH);
 }
